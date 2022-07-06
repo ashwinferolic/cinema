5756339234474024550b778cc80bb9e5d1989164
ashwinferolic <ferolicaf@gmail.com>
2022-07-06 20:02:07 +0530

    sidebar styled, need to style nav item

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+export const GET_CONFIG = "GET_CONFIG";
+export const GET_GENRES = "GET_GENRES";
+export const SET_LOADING = "SET_LOADING";
+export const REMOVE_LOADING = "REMOVE_LOADING";
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { init } from "../actions";
+import Sidebar from "./Sidebar";
+
+import Discover from "./Discover";
+import Genre from "./Genre";
 
 const App = () => {
-  return <div>App </div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(init());
+  }, [dispatch]);
+
+  return (
+    <BrowserRouter>
+      <Sidebar />
+      <Routes>
+        <Route path="/discover/:name" element={<Discover />} />
+        <Route path="/genres/:name" element={<Genre />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
