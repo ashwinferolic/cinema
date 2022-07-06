e2dc10b77c25575c738d2559f726c49914cd3d46
ashwinferolic <ferolicaf@gmail.com>
2022-07-06 09:40:38 +0530

    redux store and styled components initial setups

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import theme from "./utils/theme";
+import store from "./store";
+import GlobalStyle from "./utils/globals";
 import App from "./containers/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
+  </Provider>
 );
